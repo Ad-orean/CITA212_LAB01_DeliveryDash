47d432233f8f8c8f7ea3db2a3b438085152134b6
Ad-orean <Evans000@morrisville.edu>
2026-09-15 22:48:46 -0400

    keyboard Input and If statments
    Added Keyboard WASD input read by Debug.Log and if statements

--- Tyler Evans_CITA212_DeliveryDash/Assets/Driver.cs
+++ Tyler Evans_CITA212_DeliveryDash/Assets/Driver.cs
@@ -1,18 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
 
     [SerializeField] float steerSpeed = 0.5f;
     [SerializeField] float moveSpeed = .25f;
-        void Start()
-    {
-        
-    }
-
-   
+       
     void Update()
     {
+        if(Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("We are pushing Forward");
+        }
+
+         else if(Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("We are pushing Left");
+        }
+
+         if(Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("We are pushing backwards");
+        }
+
+         else if(Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("We are pushing Right");
+        }
+        
+        
+        
+        
+        
         transform.Rotate(0,0,steerSpeed);
         transform.Translate(0,moveSpeed,0);
     }
