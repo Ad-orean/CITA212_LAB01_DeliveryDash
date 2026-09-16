c4726b94e50110dece5c793744bdefb204211eff
Ad-orean <Evans000@morrisville.edu>
2026-09-15 22:27:28 -0400

    Transform Translate Movment
    added Transform Translate and rotate to see capsule move

--- Tyler Evans_CITA212_DeliveryDash/Assets/Driver.cs
+++ Tyler Evans_CITA212_DeliveryDash/Assets/Driver.cs
@@ -10,6 +10,7 @@ public class Driver : MonoBehaviour
    
     void Update()
     {
-        transform.Rotate(0,0,0.02f);
+        transform.Rotate(0,0,0.5f);
+        transform.Translate(0,.05f,0);
     }
 }
