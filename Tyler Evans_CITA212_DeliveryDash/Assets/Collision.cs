a91428692cbb414ee201a781118310832cc0fe6e
Ad-orean <Evans000@morrisville.edu>
2026-09-15 23:38:17 -0400

    My improvement
    My improvement was when the Player collides with the circle it is destroyed

--- Tyler Evans_CITA212_DeliveryDash/Assets/Collision.cs
+++ Tyler Evans_CITA212_DeliveryDash/Assets/Collision.cs
@@ -5,6 +5,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("you hit something!");
+        Destroy(collision.gameObject);
+    
     }
 
     void OnTriggerEnter2D(Collider2D collision)
