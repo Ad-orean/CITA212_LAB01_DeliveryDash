8393aef95e957bd60a6058c0f9ab5da42dd10d32
Ad-orean <Evans000@morrisville.edu>
2026-09-23 13:28:08 -0400

    My Improvment/polish

--- Tyler Evans_CITA212_DeliveryDash/Assets/Scripts/Driver.cs
+++ Tyler Evans_CITA212_DeliveryDash/Assets/Scripts/Driver.cs
@@ -3,23 +3,55 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+
 public class Driver : MonoBehaviour
 {
-
     [SerializeField] float steerSpeed = 0.5f;
     [SerializeField] float currentSpeed = .25f;
     [SerializeField] float boostSpeed = 10f;
     [SerializeField] float regularSpeed = 5f;
+    [SerializeField] float deliverySpeed = 7f;
 
     [SerializeField] TMP_Text boostText;
+    [SerializeField] TMP_Text statusText;
+
+    [SerializeField] Color normalColor = Color.white;
+    [SerializeField] Color packageColor = Color.yellow;
+    [SerializeField] Color deliveredColor = Color.green;
+
+    SpriteRenderer sr;
+    bool hasPackage = false;
 
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        sr.color = normalColor;
+
         boostText.gameObject.SetActive(false);
+        statusText.text = "";
     }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Boost"))
+        if (collision.CompareTag("Package"))
+        {
+            hasPackage = true;
+            Destroy(collision.gameObject);
+            statusText.text = "Package Picked Up!";
+            currentSpeed = regularSpeed;
+            sr.color = packageColor;
+        }
+
+        if (collision.CompareTag("Customer") && hasPackage)
+        {
+            hasPackage = false;
+            Destroy(collision.gameObject);
+            statusText.text = "Delivery Complete!";
+            currentSpeed = deliverySpeed;
+            sr.color = deliveredColor;
+        }
+
+        if (collision.CompareTag("Boost"))
         {
             currentSpeed = boostSpeed;
             boostText.gameObject.SetActive(true);
@@ -27,45 +59,36 @@ public class Driver : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-        
+    void OnTriggerExit2D(Collider2D collision)
     {
-       currentSpeed = regularSpeed;
-        boostText.gameObject.SetActive(false);
+        if (collision.CompareTag("Slow"))
+        {
+            currentSpeed = regularSpeed;
+        }
+    }
 
-        }    
-        
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentSpeed = regularSpeed;
+        boostText.gameObject.SetActive(false);
+        sr.color = normalColor;
+    }
+
     void Update()
     {
         float steer = 0f;
         float move = 0f;
 
-        if(Keyboard.current.wKey.isPressed)
-        {
-            move = 1f;
-        }
+        if (Keyboard.current.wKey.isPressed) move = 1f;
+        else if (Keyboard.current.sKey.isPressed) move = -1f;
 
-         else if(Keyboard.current.sKey.isPressed)
-        {
-            move = -1f;
-        }
+        if (Keyboard.current.aKey.isPressed) steer = 1f;
+        else if (Keyboard.current.dKey.isPressed) steer = -1f;
 
-         if(Keyboard.current.aKey.isPressed)
-        {
-           steer = 1f;
-        }
-
-         else if(Keyboard.current.dKey.isPressed)
-        {
-            steer = -1f;
-        }
-        
-        
         float moveAmount = move * currentSpeed * Time.deltaTime;
         float steerAmount = steer * steerSpeed * Time.deltaTime;
 
-
-        transform.Translate(0,moveAmount,0 );
-        transform.Rotate(0,0,steerAmount);
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, steerAmount);
     }
 }
